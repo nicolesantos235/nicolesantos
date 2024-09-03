375f689b775ad342449171f637b0ff6e3cd60d51
nicolesantos235 <n.santos01@escola.pr.gov.br>
2024-09-03 19:41:43 -0300

    nick

---
+++ sketch.js
@@ -0,0 +1,22 @@
+let xbolinha=300
+let ybolinha=200
+let diametro=15
+let velocidadexbolinha=5
+let velocidadeybolinha=5
+width=600
+height=400
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(0);
+  circle(xbolinha,ybolinha,diametro);
+    //xbolinha+=velocidadexbolinha;
+    ybolinha+=velocidadeybolinha;
+}
+  if (xbolinha > width|| xbolinha<0)
+    velocidadexbolinha*=-1;
+    if (ybolinha >height  || ybolinha<0){
+    velocidadeybolinha*=-1;
+} 
